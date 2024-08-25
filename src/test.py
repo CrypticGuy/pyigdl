f0be2ba245a0c3e4b2370539b9ca6c1da93bf194
Vasu Goel <vasu18322@iiitd.ac.in>
2024-08-25 17:13:02 +0530

    Changes for support thumbnail links for carousel

--- src/test.py
+++ src/test.py
@@ -1,9 +1,14 @@
 import unittest
 
-from .pyigdl import IGDownloader
+from pyigdl import IGDownloader
 
 class Test(unittest.TestCase):
     def test_execution(self):
         url = "https://www.instagram.com/reel/C-0CTl6o7SO"
         responseSize = len(IGDownloader(url))
         self.assertEqual(responseSize, 1)
+    
+    def test_carousel(self):
+        url = "https://www.instagram.com/p/C_B03_ry0aK/?"
+        resp = IGDownloader(url)
+        self.assertEqual(len(resp), 9)
